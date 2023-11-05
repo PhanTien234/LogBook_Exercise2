ff539bed82c9f10b2e1a06bfbd2b028f43203ee2
PhanTien234 <minhtienp328@gmail.com>
2023-11-05 23:55:00 +0700

    done log book 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.logbook_exercise2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.logbook_exercise2"
         minSdk = 29
-        targetSdk = 29
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -29,14 +29,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/example/logbook_exercise2/MainActivity.java
+++ app/src/main/java/com/example/logbook_exercise2/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.logbook_exercise2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.TextView;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private GifImageView imageView;
+    private TextView imageIDTextView;
+    private List<Integer> imageResources;
+    private int currentImageIndex = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        imageIDTextView = findViewById(R.id.imageID);
+        imageResources = getImageResources();
+    }
+    // Method to update the displayed image
+    private void updateImage() {
+        int imageResourceId = imageResources.get(currentImageIndex);
+        imageView.setImageResource(imageResourceId);
+        // Set the image ID in the TextView
+        int imageID = currentImageIndex;
+        imageIDTextView.setVisibility(View.VISIBLE);
+        imageIDTextView.setText("Image ID: " + imageID);
+    }
+    // Button click handler for displaying the next image
+    public void onNextButtonClick(View view) {
+        if (currentImageIndex < imageResources.size() - 1) {
+            currentImageIndex++;
+        } else {
+            currentImageIndex = 0;
+        }
+        updateImage();
+    }
+    // Button click handler for displaying the previous image
+    public void onPreviousButtonClick(View view) {
+        if (currentImageIndex > 0) {
+            currentImageIndex--;
+        } else {
+            currentImageIndex = imageResources.size() - 1;
+        }
+        updateImage();
+    }
+
+    // Method to dynamically get the list of image resources from the drawable folder
+    private List<Integer> getImageResources() {
+        List<Integer> imageResources = new ArrayList<>();
+        Field[] drawables = R.drawable.class.getFields();
+
+        TypedValue typedValue = new TypedValue();
+        for (Field field : drawables) {
+            try {
+                getResources().getValue(field.getInt(null), typedValue, true);
+                if (typedValue.string.toString().endsWith(".jpg")
+                        || typedValue.string.toString().endsWith(".png")
+                        || typedValue.string.toString().endsWith(".gif")) {
+                    int resourceId = field.getInt(null);
+                    imageResources.add(resourceId);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return imageResources;
     }
 }
